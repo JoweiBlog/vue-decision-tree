b3a4a7e2d27a54bf132f69bfa1779ae66e86c49c
jowei_xu <jowei_xu@jiliguala.com>
2021-01-29 16:23:30 +0800

    chore: update config

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/vue-decision-tree" : "/"
+  publicPath:
+    process.env.NODE_ENV === "production" ? "/vue-decision-tree" : "/",
+  css: {
+    extract: false
+  }
 };
